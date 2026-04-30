881d59c30235809a2942b9bf86a16d4a73b39180
이윤서 <yyoo04@naver.com>
2026-04-30 23:57:16 +0900

    Feat: 랜딩 페이지 구현

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,15 @@
+// app/page.tsx
+import NetflixAnimatedLogo from "@/components/NetflixLogoHandler";
+
 export default function LandingPage() {
-  return null;
+  return (
+    <main className="relative min-h-screen bg-black">
+      <div>
+        <NetflixAnimatedLogo />
+      </div>
+
+      {/* 랜딩페이지 메인 콘텐츠 영역 */}
+      <div className="flex h-screen items-center justify-center"></div>
+    </main>
+  );
 }
